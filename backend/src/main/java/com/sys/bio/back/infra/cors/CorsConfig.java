7a7e5255ab8fb3e49fdd11518b85ef8667cf46ce
Ignacio Álvarez <133690298+ignacioaalvarezc@users.noreply.github.com>
2025-03-21 15:38:02 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/sys/bio/back/infra/cors/CorsConfig.java
+++ backend/src/main/java/com/sys/bio/back/infra/cors/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://softbio.cl");
+        config.addAllowedOrigin("https://www.softbio.cl");
         //config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
