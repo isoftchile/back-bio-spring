3bf401c5a577178acf9397df4a6e80db4d951773
isoftchile <system@isoft.cl>
2025-03-21 22:30:32 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/sys/bio/back/infra/cors/CorsConfig.java
+++ backend/src/main/java/com/sys/bio/back/infra/cors/CorsConfig.java
@@ -15,7 +15,6 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://softbio.cl");
         config.addAllowedOrigin("https://www.softbio.cl");
         //config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedHeader("*");
